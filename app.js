5065f16130e0d70b39dc024639e44f933a1ea7cd
Dmytro <azarenkov.mintymint@gmail.com>
2025-10-22 23:03:02 -0400

    Refactor app.js

--- app.js
+++ app.js
@@ -40,9 +40,6 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
-// app.get('/', (req, res) => {
-//   res.send('<h1>Appointment API</h1><a href="/api-docs">Documentation</a>');
-// });
 app.use(express.static("public"));
 
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
